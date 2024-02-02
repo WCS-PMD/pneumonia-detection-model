c086eb8555757d1b30b37b35216ee181943bc9da
declankorda <dkorda@protonmail.com>
2024-02-02 17:28:07 -0500

    training and such

--- preprocessing.py
+++ preprocessing.py
@@ -1,9 +1,31 @@
 import cv2
 import os
 import tensorflow as tf
+from tensorflow.keras import models, layers
 import pathlib
 import numpy as np
 import matplotlib.pyplot as plt
+
+class pneumonia_detection_model(models.Sequential()):
+    @tf.function
+    def train_step(data):
+        print('train step')
+        images, labels = data
+
+        print("Image shape:", images.shape)
+        print("Label shape:", labels.shape)
+
+        with tf.GradientTape() as tape:
+            predictions = model(images, training=True)
+            loss = loss_object(labels, predictions)
+
+        gradients = tape.gradient(loss, model.trainable_variables)
+        optimizer.apply_gradients(zip(gradients, model.trainable_variables))
+
+        train_loss(loss)
+        train_accuracy(labels, predictions)
+
+        print("Loss:", loss.numpy())  # Add this line to print the loss during training
 
 
 def get_label(file_path: str):
@@ -11,8 +33,10 @@ def get_label(file_path: str):
     parent = parsed[-2]
     file_name = parsed[-1]
     if parent == "PNEUMONIA":
-        return tf.strings.split(file_name, '_')[1]
-    return parent
+        label_str = tf.strings.split(file_name, '_')[1]
+    else:
+        label_str = parent
+    return tf.argmax(label_str == class_names)
 
 
 def decode_img(img):
@@ -48,34 +72,77 @@ def process_path(file_path):
     return img, label
 
 
-path_to_dir = os.environ.get("DATASET_PATH")
 
-if path_to_dir is None:
-    os.environ["DATASET_PATH"] = input("Enter complete path to the chest_xray folder: ")
-    path_to_dir = os.environ["DATASET_PATH"]
-    print("You should set the DATASET_PATH enviroment variable to avoid this every time you run the program.")
 
-ds_files = [str(file.absolute()) for file in pathlib.Path(path_to_dir).glob("**/[!._]*.jpeg")]
+if __name__ == "__main__":
+    path_to_dir = os.environ.get("DATASET_PATH")
 
-list_ds = tf.data.Dataset.from_tensor_slices(ds_files)
-image_count = len(ds_files)
-list_ds = list_ds.shuffle(image_count, reshuffle_each_iteration=False)
+    if path_to_dir is None:
+        os.environ["DATASET_PATH"] = input("Enter complete path to the chest_xray folder: ")
+        path_to_dir = os.environ["DATASET_PATH"]
+        print("You should set the DATASET_PATH enviroment variable to avoid this every time you run the program.")
 
-for f in list_ds.take(5):
-    print(f.numpy())
+    ds_files = [str(file.absolute()) for file in pathlib.Path(path_to_dir).glob("**/[!._]*.jpeg")]
 
-class_names = np.array(['NORMAL', 'bacteria', 'virus'])
+    list_ds = tf.data.Dataset.from_tensor_slices(ds_files)
+    image_count = len(ds_files)
+    list_ds = list_ds.shuffle(image_count, reshuffle_each_iteration=False)
 
-val_size = int(image_count * 0.2)
-train_ds = list_ds.skip(val_size)
-val_ds = list_ds.take(val_size)
+    for f in list_ds.take(5):
+        print(f.numpy())
 
-# Set `num_parallel_calls` so multiple images are loaded/processed in parallel.
-train_ds = train_ds.map(process_path, num_parallel_calls=tf.data.AUTOTUNE)
-val_ds = val_ds.map(process_path, num_parallel_calls=tf.data.AUTOTUNE)
+    class_names = np.array(['NORMAL', 'bacteria', 'virus'])
 
-for image, label in train_ds.take(3):
-    print("Image shape: ", image.numpy().shape)
-    print("Label: ", label.numpy())
-    plt.imshow(image.numpy())
-    plt.show()
+    val_size = int(image_count * 0.2)
+    train_ds = list_ds.skip(val_size)
+    val_ds = list_ds.take(val_size)
+
+    # Set `num_parallel_calls` so multiple images are loaded/processed in parallel.
+    train_ds = train_ds.map(process_path, num_parallel_calls=tf.data.AUTOTUNE)
+    val_ds = val_ds.map(process_path, num_parallel_calls=tf.data.AUTOTUNE)
+
+    for image, label in train_ds.take(3):
+        print("Image shape: ", image.numpy().shape)
+        print("Label: ", label.numpy())
+        print("Label Shape: ", label.numpy().shape)
+        print("Type: ", class_names[label.numpy()])
+        plt.imshow(image.numpy())
+        plt.show()
+
+    model = models.Sequential()
+    model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(64, 64, 1)))
+    model.add(layers.MaxPooling2D((2, 2)))
+    model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+    model.add(layers.MaxPooling2D((2, 2)))
+    model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+    model.add(layers.Flatten())
+    model.add(layers.Dense(64, activation='relu'))
+    model.add(layers.Dense(3, activation='softmax'))
+
+    model.summary()
+
+    model.compile(optimizer='adam',
+                  loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=False),
+                  metrics=['accuracy'])
+
+    history = model.fit(train_ds, epochs=10,
+                        validation_data=val_ds)
+
+    plt.plot(history.history['accuracy'], label='accuracy')
+    plt.plot(history.history['val_accuracy'], label='val_accuracy')
+    plt.xlabel('Epoch')
+    plt.ylabel('Accuracy')
+    plt.ylim([0.5, 1])
+    plt.legend(loc='lower right')
+
+    test_loss, test_acc = model.evaluate(val_ds, verbose=2)
+
+
+
+
+
+
+
+
+
+
