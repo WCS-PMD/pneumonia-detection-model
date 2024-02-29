e4bd98c1a50f9433cadaf56a5c8ba5811c516a61
NathanWan23 <Nathan.wan23@ycdsbk12.ca>
2024-02-29 18:43:56 -0500

    IGNORE

---
+++ apps.js
@@ -0,0 +1,18 @@
+
+function MyButton() {
+    return (
+      <button>
+        See More!
+      </button>
+    );
+  }
+  
+  export default function MyApps() {
+    return (
+      <div>
+        <h1>Welcome to my app</h1>
+        <MyButton />
+      </div>
+    );
+  }
+  
